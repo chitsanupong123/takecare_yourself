1edd9427074e2be7849d46712b9063ed7f9ed071
chitsanupong123 <64314113+chitsanupong123@users.noreply.github.com>
2022-03-04 20:01:16 +0700

    routerex

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -1,5 +1,25 @@
 <template>
-  <q-page>this is newspage</q-page>
+  <q-page class="justify-center row"
+    >this is newspage
+    <div
+      class="full-width row wrap justify-between items-center content-center box"
+    >
+      <q-btn
+        :to="{ name: 'Home' }"
+        class="bg-green menu-box justify-center items-center"
+      >
+        <q-img
+          src="img/Picture1.png"
+          class="imgsize items-center content-center"
+        ></q-img>
+      </q-btn>
+      <div class="bg-green menu-box"></div>
+      <div class="bg-green menu-box"></div>
+      <div class="bg-green menu-box"></div>
+      <div class="bg-green menu-box"></div>
+      <div class="bg-green menu-box"></div>
+    </div>
+  </q-page>
 </template>
 
 <script>
@@ -9,4 +29,24 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style>
+.imgsize {
+  width: 60px;
+}
+
+.box {
+  width: 100%;
+  max-width: 800px !important;
+}
+
+.menu-box {
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: row;
+  overflow: auto;
+  min-height: 100px;
+  max-height: 100px;
+  min-width: 100px;
+  max-width: 100px;
+}
+</style>
